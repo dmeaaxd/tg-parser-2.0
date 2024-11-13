0bec0eb9f6de3e619396f4e63a9fee11c255f52e
Maksim Ushakov <dmeaaxd@gmail.com>
2024-11-13 10:37:50 +0300

    add source_username to the message

--- modules/telegram_module.py
+++ modules/telegram_module.py
@@ -50,6 +50,9 @@ async def send_message_to_target_channel(client, source_username, sender_name, m
     message = message_text
     if sender_name:
         message += f"\n\n**Для связи писать сюда:** {sender_name}"
+    else:
+        message += f"\n\n**Услуга предоставляется:** @{source_username}"
+
     topic_id = await find_target_id_by_username(source_username)
 
     if media_list:
